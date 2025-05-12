d990cff66d7d36a4e506caaa42842147ecf6d2aa
Alla0314VV <soforstudy1403@gmail.com>
2025-05-12 08:47:44 +0300

    Update square_every_digit.py

--- square_every_digit.py
+++ square_every_digit.py
@@ -5,10 +5,3 @@ def square_digits(num):
     for digit in str(num):
         result += str(int(digit)**2)
     return int(result)
-
-@test.describe("Premade tests: ")
-def fixed_tests():
-    @test.it('Basic Test Cases')
-    def basic_test_cases():
-        test.assert_equals(square_digits(9119), 811181)
-        test.assert_equals(square_digits(0), 0)
